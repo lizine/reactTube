0c9ac0d4be60415ebe829e6b51b91db9405dc2ba
Nina <nina.liljeblad@gmail.com>
2016-03-24 17:11:46 +0200

    fleshing out searchbar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Heppli!</div>
+import SearchBar from './components/search_bar'
 
+const API_KEY = 'AIzaSyDv0cVJLr3Amwx_G15padXGr8co0N07l1I';
+
+const App = () => {
+  return (
+    <div>
+     <SearchBar />
+    </div>
+  );
 };
+
+
 ReactDOM.render(<App/>, document.getElementById('container'));
