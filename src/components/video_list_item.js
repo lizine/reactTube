84e4e78bd4a72564f6e8d90457c7d14e3d062640
Nina <nina.liljeblad@gmail.com>
2016-03-25 15:25:02 +0200

    constructing video list and list item

---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// {video} is the same as const video = props.video
+const VideoListItem = ({video}) => {
+  return <li>{props.video.id.videoId}</li>
+
+};
+export default VideoListItem;
