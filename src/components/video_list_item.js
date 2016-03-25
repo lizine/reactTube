25527d7f815d68b74c8b213adaad793c30ce63a6
Nina <nina.liljeblad@gmail.com>
2016-03-25 17:23:06 +0200

    passing videoselect callback from index.js to videolist item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 // {video} is the same as const video = props.video
-const VideoListItem = ({video}) => {
+//we are just pulling stuff from props
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   const videoTitle = video.snippet.title;
   const videoUrl = video.snippet.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
 
      <div className="video-list media">
        <div className="media-left">
