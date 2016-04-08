977b0bb62d0586fcb8fac59a08c61b53d7dc2144
Nina <nina.liljeblad@gmail.com>
2016-04-08 12:58:10 +0300

    search function refactoring

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-const API_KEY = '';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDv0cVJLr3Amwx_G15padXGr8co0N07l1I';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { videos: []};
-
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-      this.setState({videos: data});
-      console.log(data[0]);
-    });
+    this.state = {
+      term: 'kittens',
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('kittens');
   }
-
+videoSearch(term) {
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo : videos[0]
+    });
+    console.log(videos[0]);
+  });
+}
   render(){
+    //we only want to fire search function every 300 ms
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
    return (
     <div>
-     <SearchBar />
-     <VideoList videos={this.state.videos} />
+     <SearchBar onSearch={videoSearch} />
+     <VideoDetail video={this.state.selectedVideo}/>
+     <VideoList
+       onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+       videos={this.state.videos} />
     </div>
    );
   }
